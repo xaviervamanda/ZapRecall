c3df32b94373b61d8668c2e58574551ef6bbfad0
xaviervamanda <amandavxavier@gmail.com>
2023-03-06 13:55:24 -0300

    Detalhes no layout

--- src/components/FlashCard.js
+++ src/components/FlashCard.js
@@ -79,6 +79,7 @@ const Back = styled.div`
     margin-bottom: 25px;
     display: flex;
     position: relative;
+    background-color: #FFFFD5;
 `
 const Question = styled.div`
     width: 270px;
@@ -101,18 +102,17 @@ const IconBack = styled(BsFillArrowRightCircleFill)`
 `
 const Container = styled.div`
     width: 300px;
-    height: 131px;
-    background-color: #FFFFFF;
+    height: 161px;
     box-shadow: 0px 4px 5px rgba(0, 0, 0, 0.15);
     border-radius: 5px;
     margin-bottom: 25px;
-    /* mudar esse display para flex quando for pra aparecer */
     display: flex;
     position: relative;
+    background-color: #FFFFD5;
 `
 const AnswerText = styled.div`
     width: 270px;
-    height: 69px;
+    height: 89px;
     margin-left: 15px;
     margin-top: 18px;
     font-family: 'Recursive';
@@ -121,6 +121,7 @@ const AnswerText = styled.div`
     font-size: 18px;
     line-height: 22px;
     color: #333333;
+    
 `
 const Options = styled.div`
     width: 274px;
